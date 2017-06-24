4d4aabad806eb6abfd1bfe129ac9e1b7911faf65
aweig <awei@aweideMacBook-Air.local>
2017-06-25 00:24:54 +0800

    11

--- pages/request/request.js
+++ pages/request/request.js
@@ -15,7 +15,7 @@ Page({
   onLoad: function (options) {
     var that = this;
     wx.request({
-      url: app.globalData.siteBaseUrl+'/weapp/request.php', //仅为示例，并非真实的接口地址
+      url: app.globalData.siteBaseUrl+'/weapp/request.php', //app.globalData.siteBaseUrl为通信域名，本人在app.js定义了
       data: {},
       header: {
         'content-type': 'application/json'
